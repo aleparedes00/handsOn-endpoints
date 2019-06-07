4e309765d670cbb02b0292434e83ae076c30e3d8
Thibault Lengyel <thibault.lengyel@devoteamgcloud.com>
2019-06-07 11:16:07 +0200

    rename spec

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ app = connexion.App(
     options={"swagger_ui": False}
 )
 
-app.add_api('contrat.yaml', strict_validation=True, validate_responses=False)
+app.add_api('openapi.yaml', strict_validation=True, validate_responses=False)
 
 application = app.app
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    
